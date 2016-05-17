8a448351ddf8d8ca53152d38b690494d0a0b83eb
William Cheaqui <willcheaqui@yahoo.com>
2016-05-17 11:21:15 -0700

    add taste from home

---
+++ js/main.js
@@ -0,0 +1,7 @@
+//listen for when the user clicks on the icon
+    $('.hamburger').on('click', function(){
+      //if menu is closed, slide down
+
+      //if menus is open, slide up
+      $('.menu').slideToggle();
+    });
